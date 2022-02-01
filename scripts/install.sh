34f552171a88b3ef1e9f0c7e881dacc31db3eb57
Florian Nitschmann <f.nitschmann@googlemail.com>
2022-02-01 13:57:56 +0100

    Fix: Build setup to support Darwin ARM64 (#26)

--- scripts/install.sh
+++ scripts/install.sh
@@ -33,6 +33,7 @@ case $(uname -m) in
     i386)   architecture="386";;
     i686)   architecture="386";;
     x86_64) architecture="amd64";;
+    arm64)  architecture="arm64";;
     arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm";;
 esac
 
